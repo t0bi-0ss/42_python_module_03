ca5a64acff1e06eb321eae8c98c4c9d8ff153f2d
t0bi-0ss <183855820+t0bi-0ss@users.noreply.github.com>
2026-06-21 01:38:57 +0200

    Write required program

---
+++ ex0/ft_command_quest.py
@@ -0,0 +1,15 @@
+import sys
+
+if __name__ == "__main__":
+    print("=== Command Quest ===")
+    args = len(sys.argv)
+    i = 1
+    print(f"Program name: {sys.argv[0]}")
+    if len(sys.argv) < 2:
+        print("No arguments provided!")
+    else:
+        print(f"Arguments received: {len(sys.argv) - 1}")
+        while (i < args):
+            print(f"Argument {i}: {sys.argv[i]}")
+            i+=1
+    print(f"Total arguments: {len(sys.argv)}")
