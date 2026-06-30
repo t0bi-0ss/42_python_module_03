ace37b8b802f4d3f6dfefa1b9e25870cdbd3406a
Tobias Sordo Ostolaza <tsordo-o@c1r2s4.42malaga.com>
2026-06-30 14:16:01 +0200

    Define distance_to_center and distance_between_coordinates functions

--- ex2/ft_coordinate_system.py
+++ ex2/ft_coordinate_system.py
@@ -1,10 +1,14 @@
 import math
 
+
 def get_player_pos() -> tuple[int, ...]:
     """Takes coordinates of a player's position validating the input data"""
     while True:
         coordinates = input("Enter new coordinates"
                             + " as floats in format 'x,y,z': ")
+        if not coordinates.count(','):
+            print("Invalid syntax")
+            continue
         try:
             1 / len(coordinates.split())
             i = 0
@@ -29,19 +33,39 @@ def get_player_pos() -> tuple[int, ...]:
     return tuple(coordinates)
 
 
+def distance_to_center(coordinates: tuple[float, ...]) -> float:
+    return (math.sqrt(
+        coordinates[0] ** 2
+        + coordinates[1] ** 2
+        + coordinates[2] ** 2
+    ))
+
+
+def distance_between_coordinates(
+        coordinates_1: tuple[float, ...],
+        coordinates_2: tuple[float, ...]
+        ) -> float:
+    return math.sqrt(
+        (coordinates_2[0] - coordinates_1[0]) ** 2
+        + (coordinates_2[1] - coordinates_1[1]) ** 2
+        + (coordinates_2[2] - coordinates_1[2]) ** 2
+    )
+
+
 if __name__ == "__main__":
     print("=== Game Coordinate System ===\n")
     print("Get a first set of coordinates")
-    coordinates = get_player_pos()
+    coordinates_1 = get_player_pos()
     print("Got a first tuple: ", end="")
-    print(coordinates)
+    print(coordinates_1)
     print(
         "It includes: "
-        + f"X={coordinates[0]}, "
-        + f"Y={coordinates[1]}, "
-        + f"Z={coordinates[2]}"
+        + f"X={coordinates_1[0]}, "
+        + f"Y={coordinates_1[1]}, "
+        + f"Z={coordinates_1[2]}"
     )
-    distance_to_center = math.sqrt(
-        coordinates[0] ** 2 + coordinates[1] ** 2 + coordinates[2] ** 2
-    )
-    print("Distance to center: " + f"{distance_to_center:.4f}")
+    distance_to_center_1 = distance_to_center(coordinates_1)
+    print("Distance to center: " + f"{distance_to_center_1:.4f}\n")
+    print("Get a second set of coordinates")
+    coordinates_2 = get_player_pos()
+    
