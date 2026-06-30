38f405070e95338eae53d493a8a8388c3d29da09
Tobias Sordo Ostolaza <tsordo-o@c1r2s4.42malaga.com>
2026-06-30 13:56:22 +0200

    Remove uneeded 'right syntax' check

--- ex2/ft_coordinate_system.py
+++ ex2/ft_coordinate_system.py
@@ -1,30 +1,10 @@
 import math
 
-
-def no_consecutive_commas(string: str) -> int:
-    if string.count(",,"):
-        return 0
-    return 1
-
-
-def has_right_syntax(string: str) -> int:
-    if not no_consecutive_commas(string):
-        return 0
-    commas_found = string.count(',')
-    if commas_found:
-        words_count = len(string.split(','))
-    else:
-        words_count = len(string.split(' '))
-    return (commas_found == (words_count - 1))
-
-
 def get_player_pos() -> tuple[int, ...]:
+    """Takes coordinates of a player's position validating the input data"""
     while True:
         coordinates = input("Enter new coordinates"
                             + " as floats in format 'x,y,z': ")
-        if not has_right_syntax(coordinates):
-            print("Invalid syntax")
-            continue
         try:
             1 / len(coordinates.split())
             i = 0
