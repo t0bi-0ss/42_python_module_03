77b04df0c3cd73f77043f7b197f457c0575e792f
Tobias Sordo Ostolaza <tsordo-o@c1r2s4.42malaga.com>
2026-06-30 16:08:39 +0200

    Define common_achievements() and unique_achievemets

--- ex3/ft_achivement_tracker.py
+++ ex3/ft_achivement_tracker.py
@@ -1,11 +1,9 @@
 import random
 
-from random import sample
-
 """Module for a players achievement system"""
 
 
-def gen_player_achievements(all_achievements: bool = False) -> set:
+def gen_player_achievements(all_achievements: bool = False) -> set[str]:
     achievements = {
         'Crafting Genius',
         'Strategist',
@@ -24,11 +22,30 @@ def gen_player_achievements(all_achievements: bool = False) -> set:
     if all_achievements:
         return achievements
     random_num_of_achievements = random.randint(1, len(achievements))
-    random_selection = sample(
+    random_selection = random.sample(
         list(achievements),
         random_num_of_achievements
     )
     return set(random_selection)
+
+
+def common_achievements(*achievements_sets: set[str]) -> set[str]:
+    if not achievements_sets:
+        return set()
+    return set.intersection(*achievements_sets)
+
+
+def unique_achievement(
+        target_set: set[str],
+        *rest_of_sets: set[str]
+        ) -> set[str]:
+    if (
+        not target_set or not rest_of_sets
+    ):
+        return set()
+    for a_set in rest_of_sets:
+        target_set.difference_update(a_set)
+    return target_set
 
 
 if __name__ == "__main__":
