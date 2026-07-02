a956a36a5fb348039bcd2a45dba4d02fcbe7023f
Tobias Sordo Ostolaza <tsordo-o@c1r2s4.42malaga.com>
2026-07-02 19:47:06 +0200

    Add range_min in players_dict_builder()

--- ex6/ft_data_alchemist.py
+++ ex6/ft_data_alchemist.py
@@ -38,18 +38,21 @@ def only_capitalized_list_builder(original_list: list[str]) -> list[str]:
     return [name for name in original_list if name == name.capitalize()]
 
 
-def players_dict_builder(player_names: list[str], range_max: int) -> dict[str, int]:
+def players_dict_builder(
+        player_names: list[str], range_max: int, range_min: int = 0
+        ) -> dict[str, int]:
     """Builds a dict from player_names with random scores"""
     if not player_names:
         print("List must not be empty")
         return None
     try:
         range_max + 1
+        range_min + 1
     except TypeError as msg:
-        print("Error: range_max must be an int:", msg)
+        print("Error: range_max and range_min must be ints:", msg)
         return None
     if range_max <= 0:
-        print("Range max can't be 0 or negative")
+        print("Range max and min can't be 0 or negative")
         return
     for s in player_names:
         try:
@@ -61,3 +64,6 @@ def players_dict_builder(player_names: list[str], range_max: int) -> dict[str, i
             )
             return None
     return {name: random.randint(0, range_max) for name in player_names}
+
+
+def greater_dict_builder()
