d0d69e74087be001b220700a6f75c868ed699b97
Tobias Sordo Ostolaza <tsordo-o@c1r2s4.42malaga.com>
2026-07-02 19:41:53 +0200

    Define players_dict_builder()

--- ex6/ft_data_alchemist.py
+++ ex6/ft_data_alchemist.py
@@ -38,5 +38,26 @@ def only_capitalized_list_builder(original_list: list[str]) -> list[str]:
     return [name for name in original_list if name == name.capitalize()]
 
 
-def players_dict_builder(player_names: list[str]) -> dict[str, int]:
-    
+def players_dict_builder(player_names: list[str], range_max: int) -> dict[str, int]:
+    """Builds a dict from player_names with random scores"""
+    if not player_names:
+        print("List must not be empty")
+        return None
+    try:
+        range_max + 1
+    except TypeError as msg:
+        print("Error: range_max must be an int:", msg)
+        return None
+    if range_max <= 0:
+        print("Range max can't be 0 or negative")
+        return
+    for s in player_names:
+        try:
+            s.capitalize()
+        except ValueError as msg:
+            print(
+                "Error: player_names must be list[str] only:",
+                msg
+            )
+            return None
+    return {name: random.randint(0, range_max) for name in player_names}
